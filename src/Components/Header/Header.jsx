8ee1c7a4cbb108e4c0cbf0bff19a2083ecaf6b2f
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-07-28 22:18:19 +0600

    navbar added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,26 @@
+import { Link, NavLink } from "react-router-dom";
+import "./Header.css"
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="navbar ">
+            <div className=" rounded-full flex-1">
+                <Link to={"/"}>
+                    <img
+                        className="w-36"
+                        alt="Tailwind CSS Navbar component"
+                        src="https://i.ibb.co/DLVQhWN/Logos.png" />
+                </Link>
+            </div>
+
+            <div className="flex-none">
+                <ul className=" menu-horizontal px-1 space-x-8 font-semibold">
+                    <li ><NavLink to={"/"}>Home</NavLink></li>
+                    <li><NavLink to={"/donations"}>Donation</NavLink></li>
+                    <li><NavLink to={"/statistics"}>Statistics</NavLink></li>
+
+                </ul>
+            </div>
         </div>
     );
 };
