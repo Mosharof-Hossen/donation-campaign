94815ad4c51af46f2ef0d1052ccef7d9a3be4380
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-07-29 23:10:13 +0600

    statestics section done

--- src/Components/DonationCart/DonationCart.jsx
+++ src/Components/DonationCart/DonationCart.jsx
@@ -1,19 +1,21 @@
 
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const DonationCart = ({ donation }) => {
-    const { picture } = donation;
+    const {id,title, picture,price, background, card_bg, category_bg, category } = donation;
     return (
-        <div className="card lg:card-side bg-base-100 border rounded">
-            <figure>
-                <img className='h-full'
+        <div className="card lg:card-side bg-base-100 border rounded" style={{backgroundColor:card_bg}}>
+            <figure className='md:h-full md:w-2/5 w-full'>
+                <img className='h-full w-full'
                     src={picture}
                     alt="Album" />
             </figure>
-            <div className="">
-                <h2 className="">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
-                
+            <div className="p-5 space-y-1">
+                <p className='w-fit px-2 py-1 rounded' style={{backgroundColor:category_bg , color:background}}>{category}</p>
+                <h2 className="text-2xl font-bold">{title}</h2>
+                <p className='text-base font-semibold' style={{color:background}}>${price}</p>
+                <Link to={`/view-details/${id}`}><button className='px-2 mt-1 py-1 text-white rounded' style={{backgroundColor:background}}>View Details</button></Link>
             </div>
         </div>
     );
--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
 
-const Statistics = () => {
+import React, { useCallback, useEffect, useState } from "react";
+import { PieChart, Pie, Cell } from "recharts";
+import { getStorageCart } from "../../Utilitis/LocalStorage";
+
+
+
+const COLORS = ["#00C49F", "#FF444A"];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+
+}) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
     return (
-        <div>
-            Statistics
-        </div>
+        <text
+            x={x}
+            y={y}
+            fill="white"
+            textAnchor={x > cx ? "start" : "end"}
+            dominantBaseline="central"
+        >
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
     );
 };
+export default function Statistics() {
+    const [yourDonation, setYourDonation] = useState([]);
 
-export default Statistics;
+    useEffect(() => {
+        const donation = getStorageCart();
+        setYourDonation(donation)
+    }, [])
+    const data = [
+        { name: "Your Donation", value: yourDonation.length },
+        { name: "Total Donation", value: 12 - yourDonation.length },
+    ];
+    return (
+        <div className="flex justify-center flex-col items-center">
+            <PieChart width={500} height={500}>
+                <Pie
+                    data={data}
+                    labelLine={false}
+                    label={renderCustomizedLabel}
+                    outerRadius={200}
+                    fill="#FF444A"
+                    dataKey="value"
+                    color={COLORS}
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+            </PieChart>
+            <p>Your Donation <span className='bg-[#00C49F] px-9 mx-3 rounded'></span> Total Donation <span className='bg-[#FF444A] px-9 mx-3 rounded'></span></p>
+        </div>
+    );
+}
