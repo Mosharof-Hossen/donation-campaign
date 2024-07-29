929ad04be9a5141248712ce3ae8d0d5511bc5448
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-07-29 16:17:22 +0600

    Category bug fixed

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,24 +1,20 @@
 
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 
 const Category = ({ singleCategory }) => {
     const { picture, title, category, background, card_bg, category_bg } = singleCategory;
-    console.log(singleCategory);
-
-    const bgClass = card_bg + ` card-body ` + ` bg-[${background}]`;
-    const textColorClass = category_bg + ` bg-[${background}] ` + ` w-fit px-2 py-1 rounded  ` + " font-semibold ";
+    
 
     return (
-        <div className="card card-compact rounded bg-base-100  shadow">
+        <div className="card card-compact rounded bg-base-100  shadow" style={{ backgroundColor: card_bg }} >
             <figure>
                 <img className='w-full'
                     src={picture}
                     alt="Shoes" />
             </figure>
-            <div className={bgClass} style={{}}  >
-                <p className={textColorClass} style={{ color: `${background}` }}>{category}</p>
-                <h2 className="card-title " style={{ color: `${background}` }}>{title}</h2>
+            <div className={"p-5 space-y-4"}  >
+                <p className='w-fit h-fit px-2 py-1 rounded' style={{ backgroundColor: category_bg, color: background }}>{category}</p>
+                <h2 className="font-bold text-xl" style={{ color: background }}>{title}</h2>
 
             </div>
         </div>
